dced5b98a42f3c0de33a2b9d8ac4b043971fb193
Theo Bahin <tbahin@c3r4p8.42perpignan.fr>
2025-06-17 20:51:12 +0200

    collision

--- srcs/move.c
+++ srcs/move.c
@@ -1,23 +1,56 @@
 #include "../includes/cub3d.h"
 
+int	valid_move(t_data *data, int move)
+{
+	float x;
+	float y;
+
+	x = 0;
+	y = 0;
+	if (move == 0)
+	{
+		x = data->player_x + data->dir_x * 0.04;
+		y = data->player_y + data->dir_y * 0.04;
+	}
+	else if (move == 1)
+	{
+		x = data->player_x - data->dir_x * 0.04;
+		y = data->player_y - data->dir_y * 0.04;
+	}
+	else if (move == 2)
+	{
+		x = data->player_x + data->dir_y * 0.04;
+		y = data->player_y - data->dir_x * 0.04;
+	}
+	else if (move == 3)
+	{
+		x = data->player_x - data->dir_y * 0.04;
+		y = data->player_y + data->dir_x * 0.04;
+	}
+	if (data->map[(int)y][(int)x] == '1')
+		return (0);
+	else
+		return (1);
+}
+
 void	move_player(t_data *data, int move)
 {
-	if (move == 0)
+	if (move == 0 && valid_move(data, 0))
 	{
 		data->player_x += data->dir_x * 0.04;
 		data->player_y += data->dir_y * 0.04;
 	}
-	else if (move == 1)
+	else if (move == 1 && valid_move(data, 1))
 	{
 		data->player_x -= data->dir_x * 0.04;
 		data->player_y -= data->dir_y * 0.04;
 	}
-	else if (move == 2)
+	else if (move == 2 && valid_move(data, 2))
 	{
 		data->player_x += data->dir_y * 0.04;
 		data->player_y -= data->dir_x * 0.04;
 	}
-	else if (move == 3)
+	else if (move == 3 && valid_move(data, 3))
 	{
 		data->player_x -= data->dir_y * 0.04;
 		data->player_y += data->dir_x * 0.04;
